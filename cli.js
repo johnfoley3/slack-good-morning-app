06f6dc23d129b5d67876b106a7ce75be3beb8ede
John Foley <johnfoley3@gmail.com>
2018-07-13 14:01:05 -0500

    Demo functions for posting messages and listing channels

--- cli.js
+++ cli.js
@@ -1,4 +1,26 @@
-const { WebClient } = require('@slack/client');
-const config = require('config');
+const { WebClient } = require("@slack/client");
+const config = require("config");
 
-let token = config.get('slack.token');
+const token = config.get("slack.token");
+const general = config.get("slack.channels.general");
+
+const web = new WebClient(token);
+
+function say(words, conversationId) {
+  web.chat.postMessage({ channel: conversationId, text: words, as_user: true })
+    .then((res) => {
+      console.log(`Message sent: ${res.ts}`);
+    })
+    .catch(console.error);
+}
+
+function listChannels() {
+  web.channels.list()
+    .then((res) => {
+      res.channels.forEach((channel) => console.log(`Channel name: ${channel.name} and id: ${channel.id}`));
+    })
+    .catch(console.error);
+}
+
+// listChannels();
+say("What's up?", general);
